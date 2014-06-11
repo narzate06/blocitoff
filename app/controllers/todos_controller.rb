ae93a6c54f8fc8ea3a2a692632aa081b8edb910b
Noe Arzate <narzate@gmail.com>
2014-06-10 22:49:18 -0500

    updated rspec_helper and rspec_rails

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,23 @@
+class TodosController < ApplicationController
+
+  def show
+    @todo = Todo.find params[:id]
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to @todo, notice: 'Your new TODO was saved.' 
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
+
+end
